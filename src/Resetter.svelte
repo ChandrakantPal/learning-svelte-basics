831ba13f1f5c973f2a4c358dee010611925939c6
ChandrakantPal <sanjay161097@gmail.com>
2021-06-20 17:42:13 +0530

    reset button

---
+++ src/Resetter.svelte
@@ -0,0 +1,11 @@
+<script>
+  import { count } from './stores'
+
+  const reset = () => {
+    // $count = 0
+    // or
+    count.set(0)
+  }
+</script>
+
+<button on:click={reset}>Reset Count</button>
